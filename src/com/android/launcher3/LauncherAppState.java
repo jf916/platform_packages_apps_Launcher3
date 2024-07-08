ed8435844f810c91512c9e55372ff8cd69986ca8
._______166 <retromac9x2@gmail.com>
2024-07-08 21:33:35 +0100

    Launcher3: Implement hidden & protected apps
    Signed-off-by: ._______166 <retromac9x2@gmail.com>

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -46,6 +46,8 @@ import com.android.launcher3.icons.IconCache;
 import com.android.launcher3.icons.IconProvider;
 import com.android.launcher3.icons.LauncherIconProvider;
 import com.android.launcher3.icons.LauncherIcons;
+import com.android.launcher3.lineage.trust.HiddenAppsFilter;
+import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 import com.android.launcher3.model.ModelLauncherCallbacks;
 import com.android.launcher3.notification.NotificationListener;
 import com.android.launcher3.pm.InstallSessionHelper;
@@ -182,7 +184,7 @@ public class LauncherAppState implements SafeCloseable {
         mIconProvider = new LauncherIconProvider(context);
         mIconCache = new IconCache(mContext, mInvariantDeviceProfile,
                 iconCacheFileName, mIconProvider);
-        mModel = new LauncherModel(context, this, mIconCache, new AppFilter(mContext),
+        mModel = new LauncherModel(context, this, mIconCache, new HiddenAppsFilter(mContext),
                 PackageManagerHelper.INSTANCE.get(context), iconCacheFileName != null);
         mOnTerminateCallback.add(mIconCache::close);
         mOnTerminateCallback.add(mModel::destroy);
@@ -240,6 +242,10 @@ public class LauncherAppState implements SafeCloseable {
 
     public boolean isSafeModeEnabled() {
         return mIsSafeModeEnabled;
+    }
+
+    public TrustDatabaseHelper getTrustData() {
+        return TrustDatabaseHelper.getInstance(this.mContext);
     }
 
     /**
