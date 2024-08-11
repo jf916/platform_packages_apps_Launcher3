8b6847c8f62c3d09b9b4490b3b8039bcbbae3556
._______166 <retromac9x2@gmail.com>
2024-08-11 15:59:33 +0100

    Launcher3: Reload icon pack settings fragment on resume

--- src/com/android/launcher3/icons/pack/IconPackSettingsFragment.java
+++ src/com/android/launcher3/icons/pack/IconPackSettingsFragment.java
@@ -79,6 +79,7 @@ public final class IconPackSettingsFragment extends RadioSettingsFragment {
     @Override
     public void onResume() {
         super.onResume();
+        reloadPreferences();
         getActivity().registerReceiver(broadCastReceiver, PKG_UPDATE_INTENT);
     }
 
